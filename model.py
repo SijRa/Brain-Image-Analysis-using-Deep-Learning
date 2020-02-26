ae9fe4eff029be15b75871e9546f493a1b177158
Sijan <Sijan_Rana@hotmail.com>
2020-02-26 16:02:31 +0000

    Fixed import from tensorflow-gpu to tensorflow

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-import tensorflow-gpu as tf
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Conv3D, Dropout, MaxPooling3D
 from tensorflow.keras.metrics import categorical_crossentropy
--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from model import Model
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import tensorflow-gpu as tf
+import tensorflow as tf
 
 from tensorflow.keras.callbacks import LearningRateScheduler, EarlyStopping
 
