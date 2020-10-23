7aeda008aa0c1a533f1cb4765e92d03daf7efd0c
Sijan <Sijan_Rana@hotmail.com>
2020-10-23 16:44:48 +0100

    Added preprocess scripts and model variations

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -8,10 +8,15 @@ default_test_size = 0.2
 def Stratified_KFolds_Generator(folds=default_folds):
   return StratifiedKFold(n_splits=folds, shuffle=True)
 
-def Train_Test_Split(features, labels, output_class_dict, test_size=default_test_size):
+def Train_Test_Split(features, labels, output_class_dict, test_size=default_test_size, stratify='risk'):
   mri_train, mri_test, clinical_train, clinical_test, conversion_train, conversion_test, risk_train, risk_test = train_test_split(features['mri'], features['clinical'],
-    labels['conversion'], labels['risk'], test_size=test_size, stratify=labels['risk'], shuffle=True)
+    labels['conversion'], labels['risk'], test_size=test_size, stratify=labels[stratify], shuffle=True)
   return mri_train, mri_test, clinical_train, clinical_test, conversion_train, conversion_test, risk_train, risk_test
+
+def Train_Test_Split_Auxiliary(features, labels, output_class_dict, test_size=default_test_size, stratify='class'):
+  mri_train, mri_test, clinical_train, clinical_test, class_train, class_test = train_test_split(features['mri'], features['clinical'],
+    labels['class'], test_size=test_size, stratify=labels[stratify], shuffle=True)
+  return mri_train, mri_test, clinical_train, clinical_test, class_train, class_test
 
 def One_Hot_Encode(y, num_classes):
   return to_categorical(y, num_classes)
